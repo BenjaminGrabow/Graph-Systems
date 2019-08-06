3a1bc6835534b385b5b96d20542e6c83dc6a18fa
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-06 13:08:53 +0200

    added libary

--- graph.js
+++ graph.js
@@ -1,3 +1,24 @@
+// Graph System 
+
+// - has Nodes and Edges
+
+// Search Algorithm: Breadth First Search algorithm
+// Find the shortest path between 2 nodes
+// Looks for the nearest nodes everytime
+// Example:
+// We need object which keeps track of :
+
+// Node: which is a value 
+// Edge = []
+// searched - true or false 
+// parent (keep track of parents)
+
+
+// Graph Object:
+//stores: 
+// nodes = [] (all nodes);
+// graph = {} (stores data structure about node - hash table)
+
 function Graph() {
   this.nodes = [];
   this.graph = {}; // will be hash table
--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 var data;
+
 function preload() {
 data = loadJSON('kevinbacon.json');
 }
 
 function setup() {
+ noCanvas();
+ console.log(data);
 
+//  var movies = data.movies;
+
+//  for ( var i = 0; i < movies.length; i++){
+//  var movie = movies[i].title;
+//  var cast = movies[i].cast;
+
+//  }
 }
+
+
+ 

f0c13b367a27b7afe3303a6358322bf7dee6e6d2
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-06 10:49:14 +0200

    added comments and constructor function

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // Graph System 
 
 // - has Nodes and Edges
+
+// Search Algorithm: Breadth First Search algorithm
+// Find the shortest path between 2 nodes
+// Looks for the nearest nodes everytime
+// Example:
+// We need object which keeps track of :
+
+// Node: which is a value 
+// Edge = []
+// searched - true or false 
+// parent (keep track of parents)
+
+// constructor function for node object 
+
+function Node() {
+  this.value = value;
+  this.edges = [];
+  this.searched = false;
+  this.parent = null;
+}
